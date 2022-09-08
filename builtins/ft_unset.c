cf7370c9125eddf4f8903bace4aae815b34d9630
Ferhat yigit Aydin <faydin@k1m39s06.42istanbul.com.tr>
2022-09-08 07:01:53 +0300

    faydin son

--- builtins/ft_unset.c
+++ builtins/ft_unset.c
@@ -1,6 +1,41 @@
 #include "minishell.h"
 
-void    ft_unset(char **av, char **env)
+// Bu fonksiyonda mallocla ilgili hata var onlara bakmamız lazım bi!!
+//10-15dk'lık işi var unsetin.
+
+void    ft_delete(char **env, char **tmp)
 {
-    
+    int i;
+    (void)env;
+
+    i = 0;
+    while(tmp[i])
+        i++;
+    i--;
+    free(tmp[i]);
+}
+
+void    ft_unset(char **env, char **arg)
+{
+    int     i;
+    int     j;
+    int     k;
+    char    **tmp;
+
+    j = 0;
+    while (arg[++j])
+    {
+        k = ft_strlen(arg[j]);
+        i = 0;
+        while (env[i])
+        {
+            if (ft_strncmp(env[i], arg[j], k) == 0)
+            {
+                tmp = env;
+                ft_delete(env ,tmp);
+            }
+            else
+                i++;
+        }
+    }
 }
