ef1f0e0158c075acdb27fdeb561fad775d486682
Burak Mat <ali.burak.mat@gmail.com>
2022-09-10 00:28:25 +0300

    set flag reviewed, should work now

--- set_nodes.c
+++ set_nodes.c
@@ -1,45 +1,59 @@
 #include "minishell.h"
 
-void	set_flags(t_shell *shell, t_node *node)//CHECK NULLS FROM HERE
+void	set_flags(t_shell *shell, t_node *node)
 {
 	int i;
+	char **flags;
 	char **tmp;
 
-	if (node->flags != NULL)
+	if (node->flags != NULL)//if there is flag
 	{
-		if (node->illegalflag == 1)
+		if (node->illegalflag == 1)//if flag is illegal
 		{
-			node->exec_args = malloc(sizeof(char *) * 2);
-			node->exec_args[0] = node->flags;
-			node->exec_args[1] = NULL;
-		}
-		else
-		{
-			tmp = ft_split(node->flags, ' ');//HAS ALWAYS SPACE????
-			i = 0;
-			if (**tmp)
+			if (node->exec_args == NULL)//if there is no command
 			{
-				while (tmp[i])
+				node->exec_args = malloc(sizeof(char *) * 2);
+				node->exec_args[0] = node->flags;
+				node->exec_args[1] = NULL;
+			}
+			else//if there is a command
+			{
+				tmp = malloc(sizeof(char *) * 3);
+				tmp[0] = node->exec_args[0];
+				tmp[1] = node->flags;
+				tmp[2] = node->exec_args[1];
+				free(node->exec_args);
+			}
+		}
+		else//if flags are legal
+		{
+			if (node->exec_args == NULL)//if there is no command
+			{
+				;//ERROR?!??? is it possible??
+			}
+			else//if there is command
+			{
+				free(node->exec_args[1]);//null at the end
+				free(node->exec_args);//all list
+				node->exec_args = NULL;//for safety
+				flags = ft_split(node->flags, ' ');//HAS ALWAYS SPACE????
+				i = 0;
+				while (flags[i])
 					++i;
-				node->exec_args = malloc(sizeof(char *) * i + 2);
-				node->exec_args[0] = node->command;
+				tmp = malloc(sizeof(char *) * i + 2);
+				tmp[0] = node->command;//command might be cmd_path
 				i = -1;
-				while (tmp[++i])
-					node->exec_args[i + 1] = tmp[i];
-				node->exec_args[i + 1] = tmp[i];
-				free(tmp);
+				while (flags[++i])
+					tmp[i + 1] = flags[i];
+				tmp[i + 1] = flags[i];
+				node->exec_args = tmp;
+				free(flags);
 			}
 		}
 	}
-	else
-	{
-		node->exec_args = malloc(sizeof(char *) * 2);
-		node->exec_args[0] = node->cmd_path;
-		node->exec_args[1] = NULL;
-	}
 }
 
-void	set_arguments(t_shell *shell, t_node *node)
+void	set_arguments(t_shell *shell, t_node *node)//CHECK NULLS FROM HERE
 {
 	int i;
 	int j;
@@ -79,9 +93,12 @@ void	set_arguments(t_shell *shell, t_node *node)
 
 void	set_path_name_to_execargs(t_node *node)
 {
-	node->exec_args = malloc(sizeof(char *) + 1);
-	node->exec_args[0] = node->command;
-	node->exec_args[1] = NULL;
+	if (node->command)
+	{
+		node->exec_args = malloc(sizeof(char *) + 1);
+		node->exec_args[0] = node->command;//might be path, depends on execve
+		node->exec_args[1] = NULL;
+	}
 }
 
 void	set_node(t_shell *shell, t_node *node)
@@ -93,6 +110,7 @@ void	set_node(t_shell *shell, t_node *node)
 		shell->err_code = 2;
 		print_error(shell);
 	}
+	set_path_name_to_execargs(node);
 	//set_flags(shell, node);
 
 }
