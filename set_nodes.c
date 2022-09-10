3c772120ce464dde59d6086f045c659f6b3e499c
Burak Mat <burakk.mat@gmail.com>
2022-09-10 14:00:58 +0300

    set_nodes ready

--- set_nodes.c
+++ set_nodes.c
@@ -17,37 +17,37 @@ void	set_flags(t_shell *shell, t_node *node)
 		}
 		else//if flags are legal
 		{
-				free(node->exec_args[1]);//null at the end
-				free(node->exec_args[0]);//command duplicate
-				free(node->exec_args);//all list
-				node->exec_args = NULL;//for safety
-				flags = ft_split(node->flags, ' ');//HAS ALWAYS SPACE????
-				i = 0;
-				while (flags[i])
-					++i;
-				tmp = malloc(sizeof(char *) * (i + 2));
-				tmp[0] = ft_strdup(node->command);//command might be cmd_path
-				i = 0;
-				while (flags[i] != NULL)
-				{
-					tmp[i + 1] = flags[i];
-					i++;
-				}
+			free(node->exec_args[1]);//null at the end
+			free(node->exec_args[0]);//command duplicate
+			free(node->exec_args);//all list
+			node->exec_args = NULL;//for safety
+			flags = ft_split(node->flags, ' ');//HAS ALWAYS SPACE????
+			i = 0;
+			while (flags[i])
+				++i;
+			tmp = malloc(sizeof(char *) * (i + 2));
+			tmp[0] = ft_strdup(node->command);//command might be cmd_path
+			i = 0;
+			while (flags[i] != NULL)
+			{
 				tmp[i + 1] = flags[i];
-				node->exec_args = tmp;
-				free(flags);
+				i++;
+			}
+			tmp[i + 1] = flags[i];
+			node->exec_args = tmp;
+			free(flags);
 		}
 	}
 }
 
-void	set_arguments(t_shell *shell, t_node *node)//CHECK NULLS FROM HERE
+void	set_arguments(t_shell *shell, t_node *node)
 {
 	int i;
 	int j;
 	char **tmp;
 	char **argument;
 
-	if (node->argument != NULL)//if there are argument(s)
+	if (node->argument != NULL)
 	{
 		j = 0;
 		while (node->exec_args[j])
@@ -63,7 +63,7 @@ void	set_arguments(t_shell *shell, t_node *node)//CHECK NULLS FROM HERE
 			tmp[i] = ft_strdup(node->exec_args[i]);
 			++i;
 		}
-		//free all execargs
+		freeexec_args(node);
 		j = 0;
 		while (argument[j] != NULL)
 		{
@@ -81,7 +81,7 @@ void	set_path_name_to_execargs(t_node *node)
 	if (node->command)
 	{
 		node->exec_args = malloc(sizeof(char *) + 1);
-		node->exec_args[0] = ft_strdup(node->command);//might be path, depends on execve
+		node->exec_args[0] = ft_strdup(node->command);
 		node->exec_args[1] = NULL;
 	}
 }
@@ -101,6 +101,9 @@ void	set_node(t_shell *shell, t_node *node)
 		print_error(shell, node);
 	}
 	set_path_name_to_execargs(node);
-	set_flags(shell, node);
-	set_arguments(shell, node);
+	if (node->illegalcommand != 1)
+	{
+		set_flags(shell, node);
+		set_arguments(shell, node);
+	}
 }
