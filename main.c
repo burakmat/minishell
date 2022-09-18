857b6d8a3a0a31f27fb9676db76b88679547aa47
osyalcin <osmanyalcin989@gmail.com>
2022-09-18 11:09:15 +0300

    shell global

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
 #include "minishell.h"
 
+t_shell shell;
 
 void	sig_int(int sig)
 {
 	(void)sig;
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	printf("\n");
-	rl_redisplay();
+	if (shell.totalnode > 0)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		printf("\n");
+		rl_redisplay();
+	}
 }
+
 
 int main(int argc, char **argv, char **env)
 {
 	t_lexout tolex;
-	t_shell shell;
 	char	*a;
 
 	(void)argc;
 	(void)argv;
 	shell.env = duplicate_env(env);
 	shell.pipes = NULL;
+	shell->exit_status = 0;
 	signal(SIGINT, &sig_int);
 	while (1)
 	{
